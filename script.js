6f1433aa9c86c7f2a95fef7de29df84dbe9e04ef
penguinbearking <snarendran@ucvts.org>
2018-11-06 10:29:06 -0500

    added js

---
+++ script.js
@@ -0,0 +1,13 @@
+//JS file
+
+function ConsoleHello() {
+	console.log("Hello, World!");
+}
+
+function PopUpHello() {
+	alert("Hello, World!");
+}
+
+function BrowserHello() {
+	document.getElementById("Hello").innerHTML="Hello, World!";
+}
